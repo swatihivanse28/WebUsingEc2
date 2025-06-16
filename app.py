da3be7452823dfdf7872f3b44fc7be316044806e
prafullg <89979323+prafullg@users.noreply.github.com>
2025-06-16 15:56:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import socket
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    hostname = socket.gethostname()
+    return render_template('index.html', hostname=hostname)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
